e7daa30fc19a7f5c8f11992fea0d5edb25214040
resoul <resoul.ua@icloud.com>
2024-09-03 01:04:00 +0300

    init

---
+++ songsurprise/App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  songsurprise
+//
+//  Created by resoul on 03.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .darkGray
+    }
+
+
+}
+
