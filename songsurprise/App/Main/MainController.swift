ef2d35c2a06cfc344aeeb98e6b5a312fcda2c680
resoul <resoul.ua@icloud.com>
2024-09-07 23:55:25 +0300

    update main screen

--- songsurprise/App/Main/MainController.swift
+++ songsurprise/App/Main/MainController.swift
@@ -9,12 +9,38 @@ import UIKit
 
 class MainController: UIViewController {
     
+    private lazy var orderItemOne = OrderView()
+    private lazy var orderItemTwo = OrderView()
+    private lazy var orderItemThree = OrderView()
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        setupLayout()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationController?.view.backgroundColor = .systemBackground
+        view.backgroundColor = .systemBackground
         navigationItem.title = "Create track"
+        
         let settings = UIBarButtonItem(image: UIImage(systemName: "gear"), style: .plain, target: self, action: #selector(handleButton))
         navigationItem.rightBarButtonItems = [settings]
+        
+        orderItemOne.configure(choseTrackItem)
+        orderItemTwo.configure(recordAudioMessageItem)
+        orderItemThree.configure(payOnceItem)
+        
+        orderItemOne.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleSelectGenres)))
+        orderItemTwo.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+        orderItemThree.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+    }
+    
+    private func setupLayout() {
+        let height = UIScreen.main.bounds.height - (view.safeAreaInsets.top + view.safeAreaInsets.bottom + 215)
+        view.addSubviews(orderItemOne, orderItemTwo, orderItemThree)
+        orderItemOne.constraints(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 65, left: 15, bottom: 0, right: 15), size: .init(width: 0, height: height / 3))
+        orderItemTwo.constraints(top: orderItemOne.bottomAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 50, left: 15, bottom: 0, right: 15), size: .init(width: 0, height: height / 3))
+        orderItemThree.constraints(top: orderItemTwo.bottomAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 50, left: 15, bottom: 0, right: 15), size: .init(width: 0, height: height / 3))
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -24,6 +50,16 @@ class MainController: UIViewController {
             controller.modalPresentationStyle = .overCurrentContext
             parent?.present(controller, animated: false)
         }
+    }
+    
+    @objc
+    func handleSelectGenres() {
+        navigationController?.pushViewController(GenresController(), animated: true)
+    }
+    
+    @objc
+    func handleTap() {
+        navigationController?.pushViewController(SettingsController(), animated: true)
     }
     
     @objc
