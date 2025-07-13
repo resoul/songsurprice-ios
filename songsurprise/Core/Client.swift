85fec918287d5fdf5b8d279beae1ef111e3dc9ed
resoul <resoul@resouls-MacBook-Air.local>
2025-07-13 15:17:48 +0300

    update config

--- songsurprise/Core/Client.swift
+++ songsurprise/Core/Client.swift
@@ -15,9 +15,24 @@ class Client {
     private init() {}
     
     func supabase() -> SupabaseClient {
-        let uri = ProcessInfo.processInfo.environment["PUBLIC_URI"] ?? ""
-        let anonKey = ProcessInfo.processInfo.environment["PUBLIC_ANON_KEY"] ?? ""
+        guard let dict = Bundle.main.infoDictionary else {
+            fatalError("plist file not found")
+        }
         
-        return SupabaseClient(supabaseURL: URL(string: uri)!, supabaseKey: anonKey)
+        guard let anonString = dict["PUBLIC_ANON_KEY"] as? String else {
+            fatalError("Anon key not set in plist")
+        }
+        
+        return SupabaseClient(
+            supabaseURL: URL(string: "https://lqcqxkbjgpoygmczrlap.supabase.co")!,
+            supabaseKey: anonString
+        )
     }
+    // deprecated
+//    func supabase() -> SupabaseClient {
+//        let uri = ProcessInfo.processInfo.environment["PUBLIC_URI"] ?? ""
+//        let anonKey = ProcessInfo.processInfo.environment["PUBLIC_ANON_KEY"] ?? ""
+//        
+//        return SupabaseClient(supabaseURL: URL(string: uri)!, supabaseKey: anonKey)
+//    }
 }
