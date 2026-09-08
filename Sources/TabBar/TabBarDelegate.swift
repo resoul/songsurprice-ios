6ca5452ad214c9cbb4327e2844c39003c955a932
MXMV <resoul.ua@icloud.com>
2026-09-08 22:46:39 +0300

    cleanup

--- Sources/TabBar/TabBarDelegate.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-public protocol TabBarDelegate: AnyObject {
-    func tabBar(_ tabBar: TabBarView, didSelectItem item: TabItem)
-    func tabBar(_ tabBar: TabBarView, didSelectGenre genre: GenreItem, inItem item: TabItem)
-    func tabBarDidSelectSearch(_ tabBar: TabBarView)
-    func tabBarDidSelectSettings(_ tabBar: TabBarView)
-}
